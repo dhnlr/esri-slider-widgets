2d26b1de452fcab39a7b12f65c74d0cb343f125e
dhnlr <hello@dhnlr.com>
2019-08-10 06:02:52 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const request = require('request-promise');
+
+module.exports = {
+  label: 'Esri Portal Gallery Slider',
+  addFields: [
+    {
+      name: 'portalURL',
+      label: 'URL of ArcGIS portal (include http://)',
+      type: 'url',
+      required: true
+    },
+    {
+      name: 'groupTitle',
+      label: 'Gallery group title',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'groupOwner',
+      label: 'Gallery group owner',
+      type: 'string',
+      required: true
+    }
+  ],
+  construct: function (self, options) {
+    self.on(
+      'apostrophe-pages:beforeSend',
+      'addEsriPortalGallery',
+      async function (req) {
+        // Node 8+: we can "await" anything that returns a promise, like
+        // the request-promise module does
+        const groupResponse = await request(
+          `${
+            req.data.home.body.items[0].portalURL
+          }/sharing/rest/community/groups?num=10&start=0&sortField=title&sortOrder=asc&q=title%3A%20${
+            req.data.home.body.items[0].groupTitle
+          }%20AND%20owner%3A${req.data.home.body.items[0].groupOwner}&f=json`,
+          { json: true }
+        );
+        const imageResponse = await request(
+          `${
+            req.data.home.body.items[0].portalURL
+          }/sharing/rest/search?num=999&start=0&sortField=title&sortOrder=asc&q=group%3A${
+            groupResponse.results[0].id
+          }%20%20type%3A%20*&f=json`,
+          { json: true }
+        );
+        req.data.arcgis = imageResponse.results;
+      }
+    );
+  }
+};
---
+++ public/js/always.js
@@ -0,0 +1,8 @@
+/* global Swiper */
+
+apos.define('esri-slider-widgets', {
+  extend: 'apostrophe-widgets',
+  construct: function (self, options) {
+    self.play = function ($widget, data, options) {};
+  }
+});
