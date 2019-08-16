b7283cf9f6da4f4f3879770408eadfb21c345b2c
dhnlr <hello@dhnlr.com>
2019-08-16 14:51:06 +0700

    Add feature to show widgets on columns, add feature to show widget on any pages, fix bug when group title and owner not a sigle word, polish scrollbar styling on chrome and firefox

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const request = require('request-promise');
 
+const getGalleryItem = config => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const groupResponse = await request(
+        `${
+          config.portalURL
+        }/sharing/rest/community/groups?num=10&start=0&sortField=title&sortOrder=asc&q=title%3A%20${encodeURIComponent(
+          config.groupTitle
+        )}%20AND%20owner%3A${encodeURIComponent(config.groupOwner)}&f=json`,
+        { json: true }
+      );
+      const imageResponse = await request(
+        `${
+          config.portalURL
+        }/sharing/rest/search?num=999&start=0&sortField=title&sortOrder=asc&q=group%3A${
+          groupResponse.results[0].id
+        }%20%20type%3A%20*&f=json`,
+        { json: true }
+      );
+      await resolve(imageResponse.results);
+    } catch (error) {
+      await reject(error);
+    }
+  });
+};
+
 module.exports = {
   label: 'Esri Portal Gallery Slider',
   addFields: [
@@ -27,33 +53,92 @@ module.exports = {
       'apostrophe-pages:beforeSend',
       'addEsriPortalGallery',
       async function (req) {
-        if (req.data.home.body) {
-          for (
-            let index = 0;
-            index < req.data.home.body.items.length;
-            index++
-          ) {
-            if (req.data.home.body.items[index].type == 'esri-slider') {
-              const groupResponse = await request(
-                `${
-                  req.data.home.body.items[index].portalURL
-                }/sharing/rest/community/groups?num=10&start=0&sortField=title&sortOrder=asc&q=title%3A%20${
-                  req.data.home.body.items[index].groupTitle
-                }%20AND%20owner%3A${
-                  req.data.home.body.items[index].groupOwner
-                }&f=json`,
-                { json: true }
-              );
-              const imageResponse = await request(
-                `${
-                  req.data.home.body.items[index].portalURL
-                }/sharing/rest/search?num=999&start=0&sortField=title&sortOrder=asc&q=group%3A${
-                  groupResponse.results[0].id
-                }%20%20type%3A%20*&f=json`,
-                { json: true }
-              );
-              req.data.arcgis = imageResponse.results;
-              break;
+        if (req.data.page) {
+          if (req.data.page.body) {
+            for (
+              let index = 0;
+              index < req.data.page.body.items.length;
+              index++
+            ) {
+              if (req.data.page.body.items[index].type == 'esri-slider') {
+                try {
+                  req.data.arcgis = await getGalleryItem(
+                    req.data.page.body.items[index]
+                  );
+                } catch (error) {
+                  console.error(error);
+                }
+              } else if (req.data.page.body.items[index].type == 'columns') {
+                if (req.data.page.body.items[index].column1) {
+                  for (
+                    let itemsCol1 = 0;
+                    itemsCol1 <
+                    req.data.page.body.items[index].column1.items.length;
+                    itemsCol1++
+                  ) {
+                    if (
+                      req.data.page.body.items[index].column1.items[itemsCol1]
+                        .type == 'esri-slider'
+                    ) {
+                      try {
+                        req.data.arcgis = await getGalleryItem(
+                          req.data.page.body.items[index].column1.items[
+                            itemsCol1
+                          ]
+                        );
+                      } catch (error) {
+                        console.error(error);
+                      }
+                    }
+                  }
+                }
+                if (req.data.page.body.items[index].column2) {
+                  for (
+                    let itemsCol2 = 0;
+                    itemsCol2 <
+                    req.data.page.body.items[index].column2.items.length;
+                    itemsCol2++
+                  ) {
+                    if (
+                      req.data.page.body.items[index].column2.items[itemsCol2]
+                        .type == 'esri-slider'
+                    ) {
+                      try {
+                        req.data.arcgis = await getGalleryItem(
+                          req.data.page.body.items[index].column2.items[
+                            itemsCol2
+                          ]
+                        );
+                      } catch (error) {
+                        console.error(error);
+                      }
+                    }
+                  }
+                }
+                if (req.data.page.body.items[index].column3) {
+                  for (
+                    let itemsCol3 = 0;
+                    itemsCol3 <
+                    req.data.page.body.items[index].column3.items.length;
+                    itemsCol3++
+                  ) {
+                    if (
+                      req.data.page.body.items[index].column3.items[itemsCol3]
+                        .type == 'esri-slider'
+                    ) {
+                      try {
+                        req.data.arcgis = await getGalleryItem(
+                          req.data.page.body.items[index].column3.items[
+                            itemsCol3
+                          ]
+                        );
+                      } catch (error) {
+                        console.error(error);
+                      }
+                    }
+                  }
+                }
+              }
             }
           }
         }
--- public/js/always.js
+++
@@ -1,8 +0,0 @@
-/* global Swiper */
-
-apos.define('esri-slider-widgets', {
-  extend: 'apostrophe-widgets',
-  construct: function (self, options) {
-    self.play = function ($widget, data, options) {};
-  }
-});
